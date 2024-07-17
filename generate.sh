8bd49c2564ff44715cfd06708256a23ccc991c19
nightspite <n16h75p173@gmail.com>
2024-07-17 22:21:09 +0200

    feat: update docs

--- generate.sh
+++ generate.sh
@@ -3,6 +3,6 @@
 rm -rf ./api-reference/client
 mkdir ./api-reference/client
 
-curl http://localhost:3001/swagger-json > ./api-reference/client/openapi.json
+curl https://api.bundle.social/swagger-json > ./api-reference/client/openapi.json
 
 npx @mintlify/scraping@latest openapi-file api-reference/client/openapi.json -o api-reference/client
